22d46c43e845ee0fcd51ae2d71886c858705da97
Thomas Chester <thomcchester@gmail.com>
2016-07-17 16:30:41 -0500

    client app.js is updated more

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,36 @@
-var KApp = angular.module("KApp",['ngMaterial', 'ngMessages', 'ngRoute']);
+var KApp = angular.module("KApp",[ 'ngMessages', 'ngRoute']);
 
 KApp.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){}])
+
+var KAppAdmin = angular.module("KAppAdmin",['ngMessages', 'ngRoute']);
+
+KAppAdmin.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
+  $routeProvider.
+    when("/admin", {
+      templateUrl: "views/admin.html",
+      controller: "AdminController"
+    }).
+    when("/emailList", {
+      templateUrl: "/views/partials/emailList.html",
+      controller: "EmailController"
+    }).
+    when("/setVariables", {
+      templateUrl: "/views/partials/setVariables.html",
+      controller: "SetVariablesController"
+    }).
+    otherwise({
+      redirectTo:'emailList'
+    });
+}]);
+
+var KAppRegistration = angular.module("KAppRegistration", ['ngRoute']);
+
+KAppRegistration.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
+
+}]);
+
+var KAppLogin = angular.module("KAppLogin", ['ngMaterial', 'ngRoute']);
+
+KAppLogin.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
+
+}]);
